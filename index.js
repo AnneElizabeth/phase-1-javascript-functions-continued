b60f6d8d50cb036e07a94026ee25c0acfd4e25a6
Anne <edmondson.anne@gmail.com>
2022-09-29 16:50:26 -0600

    commit tree e9c8a70f704981a3e5a958b0eb3ef3aa31b7ade8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return ('This Saturday, I want to ' + activity + '!');
+}
+
+const mondayWork = function (activity = 'go to the office') {
+    return ('This Monday, I will ' + activity + '.');
+}
+
+function wrapAdjective (stringFlair = '*') {
+    return (function (someAdjective = 'special') {
+        return ('You are ' +  stringFlair + someAdjective + stringFlair + '!');
+    });
+};
+
+
+/* function demoChain(name) {
+  const part1 = "hi";
+  return function () {
+    const part2 = "there";
+    return function () {
+      console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+    };
+  };
+}
+
+demoChain("Dr. Stephen Strange")()(); //=> HI there Dr. Stephen Strange */
